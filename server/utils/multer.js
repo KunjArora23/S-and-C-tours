3a239f0b0c4a9c5914eb43ffa3534b31183db820
Kunj Arora <kunjarora@Kunjs-MacBook-Air.local>
2025-07-20 19:42:23 +0530

    final

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const upload = multer({
   dest: "uploads/",
-  limits: { fileSize: 7 * 1024 * 1024 } // 20 MB
+  limits: { fileSize: 8 * 1024 * 1024 } // 20 MB
 });
 
 export default upload;
