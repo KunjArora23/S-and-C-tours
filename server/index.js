696b7caf1cb77d3934bf34b4833623d1398773b4
Kunj Arora <kunjarora@Kunjs-MacBook-Air.local>
2025-07-20 17:48:21 +0530

    final changes done

--- server/index.js
+++ server/index.js
@@ -38,7 +38,6 @@ app.use(cookieParser());
 app.use(cors({
   origin: ["https://www.sandctours.com"],
   credentials: true,
-  samesite: 'none',
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
